e985910e0189dc87694b65da0bade62e5d60f8be
RamonLGDaw <ramonlg.daw@gmail.com>
2023-12-05 19:56:12 +0100

    se agrega btn agregar

--- router/mascotasRutas.js
+++ router/mascotasRutas.js
@@ -6,23 +6,30 @@ const mascota = require('../model/mascota');
 router.get('/', async (req, res) => {
 
     try {
-
         const arrayMascotasDB = await mascota.find()
         // console.log(arrayMascotasDB);
         res.render('mascotas', {
-
                 arrayMascotas : arrayMascotasDB
-
         })
-
     } catch (error) {
         console.log(error);
     }   
 })
 
-
 router.get('/crear', (req, res)=>{
     res.render('crear')
+})
+
+router.post('/', async(req, res)=>{
+    const body = req.body;
+    try{
+        const mascotaDB = new mascota(body);
+        await mascotaDB.save();
+
+        res.redirect('/mascotas')
+    }catch(error){
+        console.log(error)
+    }
 })
 
 module.exports = router;
